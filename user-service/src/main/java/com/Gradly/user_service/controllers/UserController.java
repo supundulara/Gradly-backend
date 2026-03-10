126ef89e081f75a55f7e8b2b6e3ce56d48ec6529
supundulara <e20087@eng.pdn.ac.lk>
2026-03-10 17:04:02 +0530

    Add user search functionality to UserController and UserService

--- user-service/src/main/java/com/Gradly/user_service/controllers/UserController.java
+++ user-service/src/main/java/com/Gradly/user_service/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.Gradly.user_service.controllers;
 
+import com.Gradly.user_service.dto.UserResponse;
 import com.Gradly.user_service.model.User;
 import com.Gradly.user_service.service.UserService;
 import org.springframework.web.bind.annotation.*;
@@ -9,20 +10,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-
     @GetMapping
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable String id){
+    public User getUserById(@PathVariable String id) {
         return userService.getUserById(id);
+    }
+
+    @GetMapping("/search")
+    public List<UserResponse> searchUsers(@RequestParam String query) {
+        return userService.searchUsers(query);
     }
 }
--- user-service/src/main/java/com/Gradly/user_service/service/UserService.java
+++ user-service/src/main/java/com/Gradly/user_service/service/UserService.java
@@ -1,29 +1,34 @@
 package com.Gradly.user_service.service;
 
+import com.Gradly.user_service.dto.UserResponse;
 import com.Gradly.user_service.model.User;
 import com.Gradly.user_service.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
-    private final UserRepository userRepository;
 
+    private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    public User getUserById(String id){
+    public User getUserById(String id) {
         return userRepository.findById(id).orElse(null);
     }
 
-
+    public List<UserResponse> searchUsers(String query) {
+        return userRepository.findByNameContainingIgnoreCase(query)
+                .stream()
+                .map(u -> new UserResponse(u.getId(), u.getName()))
+                .collect(Collectors.toList());
+    }
 }
-
