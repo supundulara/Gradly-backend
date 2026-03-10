08b4339c7805bc31ee62c18830ee009c05421523
supundulara <e20087@eng.pdn.ac.lk>
2026-03-10 14:31:14 +0530

    Add messaging service configuration with Eureka integration and Maven wrapper

---
+++ messaging-service/src/test/java/com/Gradly/messaging_service/MessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gradly.messaging_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
